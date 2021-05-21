5fc409e17ad25cf6c5a8d28a699c13308d8d89f8
AliTarek97 <aly_helmy2015@hotmail.com>
2021-05-21 06:32:25 +0200

    adding layout component

--- web/src/components/InputField.tsx
+++ web/src/components/InputField.tsx
@@ -3,6 +3,7 @@ import {
   FormErrorMessage,
   FormLabel,
   Input,
+  Textarea,
 } from "@chakra-ui/react";
 import { useField } from "formik";
 import React, { InputHTMLAttributes } from "react";
@@ -10,24 +11,25 @@ import React, { InputHTMLAttributes } from "react";
 type InputFieldProps = InputHTMLAttributes<HTMLInputElement> & {
   label: string;
   name: string;
+  textarea?: boolean;
 };
 
-// destructured size out of props and renamed it to _ because it's unused
+// '' => false
+// 'error message stuff' => true
+
 export const InputField: React.FC<InputFieldProps> = ({
   label,
+  textarea,
   size: _,
   ...props
 }) => {
+  let InputOrTextarea = textarea ? Textarea : Input;
   const [field, { error }] = useField(props);
+  // TODO try to find out why there is an error in InputorTextarea
   return (
     <FormControl isInvalid={!!error}>
       <FormLabel htmlFor={field.name}>{label}</FormLabel>
-      <Input
-        {...field}
-        {...props}
-        id={field.name}
-        placeholder={props.placeholder}
-      />
+      <InputOrTextarea {...field} {...props} id={field.name} />
       {error ? <FormErrorMessage>{error}</FormErrorMessage> : null}
     </FormControl>
   );
